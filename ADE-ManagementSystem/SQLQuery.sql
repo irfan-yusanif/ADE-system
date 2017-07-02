55ff52641f1b7750fbaf21b504a61aa16a03e8c7
Irfan <m.irfanwatoo@gmail.com>
2017-07-02 01:39:33 -0700

    added sqlqueries file

--- ADE-ManagementSystem/SQLQuery.sql
+++
@@ -1,1 +0,0 @@
-﻿select * from AspNetUsers where Id = '8da7ba1a-9ef6-4d87-8ca3-726e0ddcee85'
--- ADE-ManagementSystem/SQLQuries.sql
+++ ADE-ManagementSystem/SQLQuries.sql
@@ -3,3 +3,4 @@ where u.Id = r.UserId and u.Id = '00a7f408-4847-41cc-b93e-ce982167faf7'
 
 select * from aspnetuserroles
 
+select * from AspNetUsers where Id = '8da7ba1a-9ef6-4d87-8ca3-726e0ddcee85'
